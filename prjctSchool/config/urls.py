99ee8afa61f152f4cb32828da39dfbe10be1efc9
loowpts <pavelbariik@yandex.ru>
2025-10-11 13:09:07 +0300

    update

--- prjctSchool/config/urls.py
+++ prjctSchool/config/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.core.urls', namespace='core')),
     path('users/', include('apps.users.urls', namespace='users')),
     path('courses/', include('apps.courses.urls', namespace='courses')),
+    path('enrollments/', include('apps.enrollments.urls', namespace='enrollments')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
